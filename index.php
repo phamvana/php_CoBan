463d42ddbb3ddfe3a2289efdd735d60118966689
Pham Van A <phamvana@gmail.com>
2023-06-15 16:54:03 +0700

    Các hàm lấy dữ liệu của PDO PDO::FETCH_BOTH PDO::FETCH_NUM PDO::FETCH_ASSOC PDO::FETCH_OBJ

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
-<?php 
-    require('bootstrap/header.php');
+<?php
+require('bootstrap/header.php');
+require_once "PDO/connect.php";
+$qr = "SELECT * FROM users";
+$query = $handler->query($qr);
 
+// while($r=$query->fetch())
+// {
+//     //Xuất kết quả từ bảng users
+//     echo $r['name'], "<br />"; 
+// }
+// $r=$query->fetch();
+// echo '<pre>', print_r($r), '</pre>';
 
+//PDO::FETCH_BOTH
+// $r=$query->fetch(PDO::FETCH_BOTH);
+// echo '<pre>', print_r($r), '</pre>';  
 
-    require_once "PDO/connect.php";
-    $qr = "SELECT * FROM users";
-    $query = $handler->query($qr);
-    
-    // while($r=$query->fetch())
-    // {
-    //     //Xuất kết quả từ bảng users
-    //     echo $r['name'], "<br />"; 
-    // }
-    $r=$query->fetch();
-    echo '<pre>', print_r($r), '</pre>';
+//PDO::FETCH_NUM
+// $r=$query->fetch(PDO::FETCH_NUM);
+// echo '<pre>', print_r($r), '</pre>';
 
-    require('bootstrap/footer.php');
+//PDO::FETCH_ASSOC
+// $r = $query->fetch(PDO::FETCH_ASSOC);
+// echo '<pre>', print_r($r), '</pre>';
 
-?>
+//PDO::FETCH_OBJ
+// $r = $query->fetch(PDO::FETCH_OBJ);
+// echo '<pre>', print_r($r), '</pre>';
+echo "<hr />";
+while($r = $query->fetch(PDO::FETCH_OBJ)){
+    echo $r->email, "<br />";
+}
+echo "<hr />";
+require('bootstrap/footer.php');
